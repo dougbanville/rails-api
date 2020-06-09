f61239fd94cf63385aecb279513ae0e12cec8917
Doug Banville <doug.banville@gmail.com>
2020-06-09 19:18:59 +0100

    tests work

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -8,10 +8,9 @@ describe ArticlesController do
       expect(response).to have_http_status(:ok)
     end
     it 'should return proper json' do
-      articles = create_list :article, 2
+      create_list :article, 2
       subject
-      pp Article.recent
-      articles.each_with_index do |article, index|
+      Article.recent.each_with_index do |article, index|
         expect(json_data[index]['attributes']).to eq({
           "title" => article.title,
           "content" => article.content,
@@ -19,12 +18,12 @@ describe ArticlesController do
         })
       end
     end
-    # it 'should return articles in the proper order' do
-    #   old_article = create :article
-    #   new_article = create :article
-    #   subject
-    #   expect(json_data.first['id'].to eq(new_article.id.to_s))
-    #   expect(json_data.last['id'].to eq(old_article.id.to_s))
-    # end
+    it 'should return articles in the proper order' do
+      old_article = create :article
+      newer_article = create :article
+      subject
+      expect(json_data.first['id']).to eq(newer_article.id.to_s)
+      expect(json_data.last['id']).to eq(old_article.id.to_s)
+    end
   end
 end
